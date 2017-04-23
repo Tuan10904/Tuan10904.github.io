88f14da36e128e86ad645d7934c1420080689789
Tuan10904 <workingtuanld@gmail.com>
2017-04-23 17:12:17 +0700

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,8 @@
+/*lighthouse studio
+Created by Lu Dinh Tuan
+Last Modified Date: */
+
+$(window).on('load',function() {
+    $("#preload").delay(700).fadeOut("slow");
+	$("#content").delay(700).removeClass("hidden");
+});
